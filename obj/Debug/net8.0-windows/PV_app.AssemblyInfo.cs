96fefb4c65150a4b09ca1d778fca203556d21954
Fer <Ferchess26@hotmail.com>
2025-08-21 14:14:02 -0600

    depuracion de proyecto

--- obj/Debug/net8.0-windows/PV_app.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PV_app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PV_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac207fef7a4f6d85e913d2d058d459ed51d1d1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PV_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PV_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
